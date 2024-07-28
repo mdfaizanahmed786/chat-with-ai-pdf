a5e3907ce3f4844de06358323f07e73cc2f4241e
Faizan Ahmed <faizanzaid789@gmail.com>
2024-07-28 19:15:34 +0530

    pdf: loaded on the client

--- components/PDFView.tsx
+++ components/PDFView.tsx
@@ -41,6 +41,63 @@ function PDFView({ url }: { url: string }) {
   };
   return (
     <div className="flex flex-col justify-center items-center">
+        <div className="sticky top-0 z-50 bg-gray-100 p-2 rounded-b-lg">
+        <div className="max-w-6xl px-2 grid grid-cols-6 gap-2">
+          <Button
+            variant="outline"
+            disabled={pageNumber === 1}
+            onClick={() => {
+              if (pageNumber > 1) {
+                setPageNumber(pageNumber - 1);
+              }
+            }}
+          >
+            Previous
+          </Button>
+
+          <p className="flex items-center justify-center">
+            {pageNumber} of {numPages}
+          </p>
+          <Button
+            variant="outline"
+            disabled={pageNumber === numPages}
+            onClick={() => {
+              if (numPages) {
+                if (pageNumber < numPages) {
+                  setPageNumber(pageNumber + 1);
+                }
+              }
+            }}
+          >
+            Next
+          </Button>
+          <Button
+            variant="outline"
+            onClick={() => setRotation((rotation + 90) % 360)}
+          >
+            <RotateCw />
+          </Button>
+
+          <Button
+            variant="outline"
+            disabled={scale >= 1.5}
+            onClick={() => {
+              setScale(scale * 1.2);
+            }}
+          >
+            <ZoomInIcon />
+          </Button>
+
+          <Button
+            variant="outline"
+            disabled={scale <= 0.75}
+            onClick={() => setScale(scale / 1.2)}
+          >
+            <ZoomOutIcon />
+          </Button>
+
+          </div>
+          </div>
       {!file ? (
         <Loader2Icon className="w-10 h-10 animate-spin" />
       ) : (
